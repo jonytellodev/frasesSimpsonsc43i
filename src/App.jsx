38fbb200f1e3b488304557d9601203b116c6b8d9
jonytellodev <jonathantello.dev@gmail.com>
2023-07-26 20:13:26 -0300

    enviamos el state personaje

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase></Frase>
+        <Frase datosPersonaje={frasePersonaje}></Frase>
         <Button variant="warning">Obtener frase</Button>
       </Container>
     </>
