34a44094179d7e49227fba1e57a458dd3ae17d89
jonytellodev <jonathantello.dev@gmail.com>
2023-07-26 21:10:24 -0300

    mostrar spinner cuando hacemos click en obtener frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
   const [frasePersonaje, setFrasePersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   //useEffect solo en montaje
   useEffect(() => {
@@ -16,29 +17,40 @@ function App() {
 
   const consultarApi = async () => {
     try {
+      setMostrarSpinner(true);
       //codigo que necesito ejecutar
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
       const dato = await respuesta.json();
       console.log(respuesta);
       console.log(dato[0]);
 
       // guardar respuesta en el state
       setFrasePersonaje(dato[0]);
-
+      //ocultar spinner
+      setMostrarSpinner(false);
     } catch (error) {
       console.log(error);
       // dejar mensaje intuitivo para el usuario final
     }
   };
 
-
-
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase datosPersonaje={frasePersonaje}></Frase>
-        <Button variant="warning" onClick={consultarApi}>Obtener frase</Button>
+
+        {mostrarSpinner === true ? (
+          <div className="m-5">
+            <Spinner animation="border" variant="primary" />
+          </div>
+        ) : (
+          <Frase datosPersonaje={frasePersonaje}></Frase>
+        )}
+        <Button variant="warning" onClick={consultarApi}>
+          Obtener frase
+        </Button>
       </Container>
     </>
   );
